83c96651dbc871235b4ebbf47dd9a6ea4275b44b
Гудков Никита Сергеевич <gudkov.nikita@bk.ru>
2021-04-06 21:23:37 +0300

    remove health

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { HealthController } from './health/health.controller';
-import { HealthService } from './health/health.service';
 import { AuthModule } from './auth/auth.module';
-import { TeachersController } from './teachers/teachers.controller';
 import { TeachersModule } from './teachers/teachers.module';
 
 @Module({
@@ -14,7 +11,5 @@ import { TeachersModule } from './teachers/teachers.module';
     AuthModule,
     TeachersModule
   ],
-  controllers: [HealthController],
-  providers: [HealthService],
 })
 export class AppModule {}
